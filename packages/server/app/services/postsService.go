4b7d08a393b57494e50b7251fd5d2c845b1b0b60
Anastasiia Lytvyn <lytvynana@gmail.com>
2023-04-04 19:58:44 +0300

    fix name for post services var

--- packages/server/app/services/postsService.go
+++ packages/server/app/services/postsService.go
@@ -10,23 +10,23 @@ type postsService struct {
 	model *models.PostModel
 }
 
-func (postService *postsService) GetAllPosts(limit int, offset int) []models.Post {
-	return postService.model.FindAll(limit, offset)
+func (postsService *postsService) GetAllPosts(limit int, offset int) []models.Post {
+	return postsService.model.FindAll(limit, offset)
 }
 
-func (postService *postsService) GetOnePost(id int) *models.Post {
-	return postService.model.FindOne(id)
+func (postsService *postsService) GetOnePost(id int) *models.Post {
+	return postsService.model.FindOne(id)
 }
 
-//func (postService *postsService) GetOneByEmail(email string) *models.Post {
-//	return postService.model.FindOneByEmail(email)
+//func (postsService *postsService) GetOneByEmail(email string) *models.Post {
+//	return postsService.model.FindOneByEmail(email)
 //}
 
-func (postService *postsService) AddPost(account *models.Account, createPostDto *dtos.CreatePostDto) {
+func (postsService *postsService) AddPost(account *models.Account, createPostDto *dtos.CreatePostDto) {
 	post := &models.Post{Account: account, Title: createPostDto.Title, Description: createPostDto.Description, PublishedAt: time.Now()}
-	postService.model.Add(post)
+	postsService.model.Add(post)
 }
 
-func (postService *postsService) RemovePost(id int) {
-	postService.model.Delete(id)
+func (postsService *postsService) RemovePost(id int) {
+	postsService.model.Delete(id)
 }
